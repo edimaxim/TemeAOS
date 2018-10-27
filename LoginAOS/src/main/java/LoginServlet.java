bcf1aa877b2c52ab54c5e509c9103327a4ba8912
edimaxim <44469139+edimaxim@users.noreply.github.com>
2018-10-27 15:57:52 +0300

    TemaNota7

---
+++ LoginAOS/src/main/java/LoginServlet.java
@@ -0,0 +1,58 @@
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String firstName = request.getParameter("firstName");
+
+		String email = request.getParameter("email");
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/AOS", "postgres", "realmadrid");
+
+			PreparedStatement statement = con
+					.prepareStatement("INSERT INTO user1 VALUES('" + firstName + "','" + email + "')");
+			statement.executeUpdate();
+			response.getWriter().append("Persoana adaugata!");
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ LoginAOS/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>index</title>
+</head>
+<body bgcolor="silver">
+	<form action="LoginServlet" method="post">
+		First name:<br> <input type="text" name="firstName"><br>
+		Last name:<br> <input type="text" name="email"> <br>
+		<input type="submit" value="SUBMIT">
+	</form>
+</body>
+</html>
